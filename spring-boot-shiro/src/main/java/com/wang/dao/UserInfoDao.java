47e92ca44333e814d92ca60fb66337c48e39ab12
wangpei <Pei@123>
2018-11-27 15:52:27 +0800

    多线程并发操作案例

--- spring-boot-shiro/src/main/java/com/wang/dao/UserInfoDao.java
+++ spring-boot-shiro/src/main/java/com/wang/dao/UserInfoDao.java
@@ -3,7 +3,11 @@ package com.wang.dao;
 
 import com.wang.model.UserInfo;
 
+import java.util.List;
+
 public interface UserInfoDao {
     /**通过username查找用户信息;*/
     UserInfo findByUsername(String username);
+
+    void batchInsert(List<UserInfo> list);
 }
--- spring-boot-shiro/src/main/java/com/wang/service/impl/UserInfoServiceImpl.java
+++ spring-boot-shiro/src/main/java/com/wang/service/impl/UserInfoServiceImpl.java
@@ -6,14 +6,79 @@ import com.wang.service.UserInfoService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
     @Resource
     private UserInfoDao userInfoDao;
+
     @Override
     public UserInfo findByUsername(String username) {
         System.out.println("UserInfoServiceImpl.findByUsername()");
         return userInfoDao.findByUsername(username);
+    }
+
+
+    public void test(List<UserInfo> list) throws Exception {
+
+        int count = 1000;
+        int listSize = list.size();
+        int runSize = (listSize / count) + 1;
+
+//        ThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+        CountDownLatch begin = new CountDownLatch(1);
+        CountDownLatch end = new CountDownLatch(runSize);
+
+        List<UserInfo> newList;
+        for (int i = 0; i < runSize; i++) {
+
+            if ((i + 1) == runSize) {
+                int startIndex = (i * count);
+                int endIndex = list.size();
+                newList = list.subList(startIndex, endIndex);
+            } else {
+                int startIndex = i * count;
+                int endIndex = (i + 1) * count;
+                newList = list.subList(startIndex, endIndex);
+            }
+
+            ExecupteHp hpRunnable = new ExecupteHp(newList, end, begin);
+            executor.execute(hpRunnable);
+
+        }
+        begin.countDown();
+        end.await();
+        executor.shutdown();
+
+    }
+
+
+    class ExecupteHp implements Runnable {
+        private List<UserInfo> list;
+        private CountDownLatch begin;
+        private CountDownLatch end;
+
+        ExecupteHp(List<UserInfo> list, CountDownLatch end, CountDownLatch begin) {
+            this.list = list;
+            this.begin = begin;
+            this.end = end;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                userInfoDao.batchInsert(list);
+                begin.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                end.countDown();
+            }
+
+        }
     }
 }
